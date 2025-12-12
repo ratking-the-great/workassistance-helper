0262a383d409864e47ad68fcce5183d1c125db90
ratking <johnnytheratking@gmail.com>
2025-12-12 15:41:39 +0100

    init

--- Workassistance-Helper/src/app/app.routes.server.ts
+++ Workassistance-Helper/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pipe/:type/size/:medium',
+    renderMode: RenderMode.SSR
+  },
+  {
+    path: 'pipe/:type',
+    renderMode: RenderMode.SSR
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- Workassistance-Helper/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'pipe/:type', loadComponent: () => import('./pages/pipe/pipe-medium.component').then(m => m.PipeMediumComponent) },
-  { path: 'pipe/:type/size/:medium', loadComponent: () => import('./pages/pipe/pipe-dimension.component').then(m => m.PipeDimensionComponent) },
-  { path: 'reparatursysteme', loadComponent: () => import('./pages/reparatursysteme/reparatursysteme.component').then(m => m.ReparatursystemeComponent) },
-];
